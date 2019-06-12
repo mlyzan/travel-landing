525f2c31443a81cad3711684febf2a88c89156a1
Volodymyr <mlyzan@ukr.net>
2019-06-12 18:28:12 +0300

    add module and collected via webpack

--- new.js
+++
@@ -1,6 +0,0 @@
-
-let age = document.getElementById('age').value;
-function showUser(surname, name) {
-	alert("Пользователь " + surname + " " + name + ", его возраст " + value);
-};
-showUser('alex', 'nomi');
